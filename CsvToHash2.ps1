abeb98df915927b32fd2e4e0f3511672916ae1f8
daisuke yamanaka <triaedz@gmail.com>
2011-05-14 20:59:31 +0900

    Changed csv read method

--- CsvToHash2.ps1
+++ CsvToHash2.ps1
@@ -1,3 +1,5 @@
+[void][System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') 
+
 function CsvToHash2 {
     param (
         [String]$csv,
@@ -6,29 +8,24 @@ function CsvToHash2 {
     
     if (Test-Path $csv -include *.csv) {
         $csv = Convert-Path $csv
-        $enc = [Text.Encoding]::GetEncoding($encoding)
-        $reader = New-Object IO.StreamReader($csv, $enc)
-        $header = $reader.Readline() -split ","
-        for ($i = 0; $i -lt $header.length; $i++) {
-            if ($header[$i] -match "`".*`"") {
-                $header[$i] = $header[$i] -replace "`"(.*)`"", "`$1"
-            }
-        }
+        $enc = [System.Text.Encoding]::GetEncoding($encoding)
+        $parser = New-Object Microsoft.VisualBasic.FileIO.TextFieldParser($csv, $enc)
+        $parser.TextFieldType = [Microsoft.VisualBasic.FileIO.FieldType]::Delimited
+        $parser.SetDelimiters(",")
+        
+        $header = $parser.ReadFields()
         $hash_all = @{}
-        while(!$reader.EndOfStream){
+        while(!$parser.EndOfData){
             $hash_line = @{}
-            $line = $reader.Readline() -split ","
+            $line = $parser.ReadFields()
             for ($j = 1; $j -lt $line.length; $j++) {
                 $key_line = $header[$j]
                 $hash_line.$key_line = $line[$j]
             }
-            if ($line[0] -match "`".*`"") {
-                $line[0] = $line[0] -replace "`"(.*)`"", "`$1"
-            }
             $key_all = $line[0]
             $hash_all.$key_all = $hash_line
         }
-        $reader.Close()
+        $parser.Close()
         return $hash_all
     } else { Write-Host "Specified file does not exist or is not csv format."}
 }
